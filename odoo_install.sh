67d9755cc88e3b8e662ec9c0230f124a05d96ba3
Yenthe <yenthevg@gmail.com>
2015-09-10 07:47:23 +0200

    Added some info
    Added some info

--- odoo_install.sh
+++ odoo_install.sh
@@ -21,12 +21,11 @@
 OE_USER="odoo"
 OE_HOME="/$OE_USER"
 OE_HOME_EXT="/$OE_USER/$OE_USER-server"
-#The default port where this Odoo instance will run under
-OE_PORT="8070"
+#The default port where this Odoo instance will run under (provided you use the command -c in the terminal)
 #Set to true if you want to install it, false if you don't need it or have it already installed.
 INSTALL_WKHTMLTOPDF="True"
 
-#Choose the Odoo version which you want to install. For example: 8.0, 7.0 or saas-6. When using 'trunk' the master version will be installed.
+#Choose the Odoo version which you want to install. For example: 9.0, 8.0, 7.0 or saas-6. When using 'trunk' the master version will be installed.
 OE_VERSION="8.0"
 
 #set the superadmin password
