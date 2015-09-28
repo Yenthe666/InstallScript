7240f034b78e36c288bde7c7f86f00513e9edfcb
Temur Vibliani <tvibliani@delta-comm.ge>
2015-09-28 15:41:47 +0400

    [FIX] !add odoo user to sudoers and !/bin/bash
     - Do NOT grant administrative access to the odoo system user
     - Restrict odoo system user to /bin/false shell.
    
    fixes Yenthe666/InstallScript#2

--- V9/odoo_install.sh
+++ V9/odoo_install.sh
@@ -107,9 +107,7 @@ else
 fi
 	
 echo -e "\n---- Create ODOO system user ----"
-adduser --system --quiet --shell=/bin/bash --home=$OE_HOME --gecos 'ODOO' --group $OE_USER
-#The user should also be added to the sudo'ers group.
-adduser $OE_USER sudo
+adduser --system --quiet --shell=/bin/false --home=$OE_HOME --gecos 'ODOO' --group $OE_USER
 
 echo -e "\n---- Create Log directory ----"
 mkdir /var/log/$OE_USER
