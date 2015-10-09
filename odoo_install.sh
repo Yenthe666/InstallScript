5f7ccdc76a2fec7da9f7f986e8ecf5e37f4c30dc
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2015-10-08 19:22:35 -0500

    Reducing the download size
    Added the "--depth 1" to the command so it only retrieves the latest version without all the repository history, which makes the download quite a bit quicker, and uses less space in disk, making it suitable for a production environment.

--- odoo_install.sh
+++ odoo_install.sh
@@ -107,7 +107,7 @@ sudo chown $OE_USER:$OE_USER /var/log/$OE_USER
 # Install ODOO
 #--------------------------------------------------
 echo -e "\n==== Installing ODOO Server ===="
-sudo git clone --branch $OE_VERSION https://www.github.com/odoo/odoo $OE_HOME_EXT/
+sudo git clone --depth 1 --branch $OE_VERSION https://www.github.com/odoo/odoo $OE_HOME_EXT/
 
 echo -e "\n---- Create custom module directory ----"
 sudo su $OE_USER -c "mkdir $OE_HOME/custom"
