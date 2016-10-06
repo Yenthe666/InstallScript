e0538167d6b1bc0d4b4075697ccdb4522fc09df8
Yenthe <yenthevg@gmail.com>
2016-10-06 05:28:30 +0200

    WIP: Add options to /etc/your-odoo-server.conf
    Needed in V10.

--- odoo_install.sh
+++ odoo_install.sh
@@ -121,6 +121,7 @@ sudo chmod 640 /etc/${OE_CONFIG}.conf
 echo -e "* Change server config file"
 sudo sed -i s/"db_user = .*"/"db_user = $OE_USER"/g /etc/${OE_CONFIG}.conf
 sudo sed -i s/"; admin_passwd.*"/"admin_passwd = $OE_SUPERADMIN"/g /etc/${OE_CONFIG}.conf
+sudo su root -c "echo '[options]'"
 sudo su root -c "echo 'logfile = /var/log/$OE_USER/$OE_CONFIG$1.log' >> /etc/${OE_CONFIG}.conf"
 sudo su root -c "echo 'addons_path=$OE_HOME_EXT/addons,$OE_HOME/custom/addons' >> /etc/${OE_CONFIG}.conf"
 
