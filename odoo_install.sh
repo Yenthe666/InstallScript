9a3f1296889247c41c174c0a2692e9b5ea2f8f69
Yenthe V.G <yenthevg@gmail.com>
2017-05-05 09:47:29 +0200

    [IMP] Create custom addons directory on community version
    This was the behaviour in 9.0 but went lost in the 10.0 script.
    This commit fixes the issue, otherwise there is no custom directory.

--- odoo_install.sh
+++ odoo_install.sh
@@ -122,11 +122,11 @@ if [ $IS_ENTERPRISE = "True" ]; then
     sudo apt-get install nodejs npm
     sudo npm install -g less
     sudo npm install -g less-plugin-clean-css
-else
-    echo -e "\n---- Create custom module directory ----"
-    sudo su $OE_USER -c "mkdir $OE_HOME/custom"
-    sudo su $OE_USER -c "mkdir $OE_HOME/custom/addons"
 fi
+
+echo -e "\n---- Create custom module directory ----"
+sudo su $OE_USER -c "mkdir $OE_HOME/custom"
+sudo su $OE_USER -c "mkdir $OE_HOME/custom/addons"
 
 echo -e "\n---- Setting permissions on home folder ----"
 sudo chown -R $OE_USER:$OE_USER $OE_HOME/*
