b12c20ee15907e6d3a6ae5038cc6ed6ba45bf975
drmedrme <drmedrme@users.noreply.github.com>
2017-09-09 20:56:41 +0100

    saas-17 change

--- odoo_install.sh
+++ odoo_install.sh
@@ -27,7 +27,8 @@ INSTALL_WKHTMLTOPDF="True"
 OE_PORT="8069"
 #Choose the Odoo version which you want to install. For example: 10.0, 9.0, 8.0, 7.0 or saas-6. When using 'trunk' the master version will be installed.
 #IMPORTANT! This script contains extra libraries that are specifically needed for Odoo 10.0
-OE_VERSION="10.0"
+#OE_VERSION="10.0"
+OE_VERSION="saas-17"
 # Set this to True if you want to install Odoo 10 Enterprise!
 IS_ENTERPRISE="False"
 #set the superadmin password
