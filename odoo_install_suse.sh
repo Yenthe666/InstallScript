b6f06cee758ad5df4d698203b94103ee87e54b0b
Paul Aswa <aswapaul@gmail.com>
2018-02-09 14:32:07 +0300

    Adapt config to  OpenSUSE
    Skip install of WKHTMLTOPDF
    
    Alter conf file name in service

--- odoo_install_suse.sh
+++ odoo_install_suse.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ################################################################################
-# Script for installing Odoo 11 on OpenSUSE 42.2 (could be used for other version too)
+# Script for installing Odoo 11 on OpenSUSE 42.3 (could be used for other versions too but WITH SOME EDITS)
 # Author: Yenthe Van Ginneken
 # Author: Aswa Paul
 #-------------------------------------------------------------------------------
@@ -47,7 +47,7 @@ OE_HOME="/$OE_USER"
 OE_HOME_EXT="/$OE_USER/${OE_USER}-server"
 #The default port where this Odoo instance will run under (provided you use the command -c in the terminal)
 #Set to true if you want to install it, false if you don't need it or have it already installed.
-INSTALL_WKHTMLTOPDF="True"
+INSTALL_WKHTMLTOPDF="False"
 #Set the default Odoo port (you still have to use -c /etc/odoo-server.conf for example to use this.)
 OE_PORT="8069"
 #Choose the Odoo version which you want to install. For example: 11.0, 10.0, 9.0 or saas-18. When using 'master' the master version will be installed.
@@ -99,11 +99,8 @@ sudo zypper addrepo https://download.opensuse.org/repositories/spins:invis:commo
 sudo zypper refresh
 sudo zypper install -y python-PyChart
 
-echo -e "\n---- Install python libraries ----"
-sudo zypper install python3-suds
-
 echo -e "\n---- Install python packages ----"
-sudo pip3 install PyPDF2 PyWebDAV
+sudo pip3 install PyPDF2 PyWebDAV suds-jurko
 sudo pip3 install python-dateutil docutils feedparser jinja2 ldap lxml mako mock
 sudo pip3 install python-openid psycopg2 psutil babel pydot pyparsing reportlab simplejson pytz 
 sudo pip3 install unittest2 vatnumber vobject pywebdav werkzeug xlwt pyyaml pypdf passlib decorator
@@ -220,7 +217,7 @@ After=network.target
 Type=simple
 User=$OE_USER
 WorkingDirectory=$OE_HOME
-ExecStart=$OE_HOME_EXT/$OE_CONFIG --config=/etc/openerp-server.conf
+ExecStart=$OE_HOME_EXT/$OE_CONFIG --config=/etc/$OE_CONFIG.conf
 
 [Install]
 WantedBy=multi-user.target
