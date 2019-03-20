e5b09cef1ff7f0e7885beef6efbc0363e3f36e36
Mohamed Sdeek <msdeek@codefish.com.eg>
2019-03-20 19:22:28 +0200

    Update odoo_install.sh

--- odoo_install.sh
+++ odoo_install.sh
@@ -51,7 +51,8 @@ sudo apt-get upgrade -y
 #----------------------------------------------------------
 #  tools and libraries required to build Odoo dependencies
 #----------------------------------------------------------
-sudo apt install git python3-pip build-essential wget python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less
+echo -e "\n---- tools and libraries required to build Odoo dependencies ----"
+sudo apt install -y libxslt1-dev git python3-pip build-essential wget python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less
 #--------------------------------------------------
 # Install PostgreSQL Server
 #--------------------------------------------------
