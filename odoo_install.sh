c8fefeca14ab1a6f72c569c1fb590eb503880383
Yenthe V.G <yenthevg@gmail.com>
2019-05-11 14:50:59 +0200

    [FIX] odoo_install: add libq-dev as dependency
    Should fix https://github.com/Yenthe666/InstallScript/issues/130#issuecomment-491508265

--- odoo_install.sh
+++ odoo_install.sh
@@ -52,7 +52,7 @@ sudo apt-get upgrade -y
 # Install PostgreSQL Server
 #--------------------------------------------------
 echo -e "\n---- Install PostgreSQL Server ----"
-sudo apt-get install postgresql -y
+sudo apt-get install postgresql libpq-dev -y
 
 echo -e "\n---- Creating the ODOO PostgreSQL User  ----"
 sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
