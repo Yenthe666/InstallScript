7f8fd7908e40a39b27f22bfe341096a51bcf7b26
Yenthe Van Ginneken <yenthevg@gmail.com>
2019-10-14 09:44:56 +0200

    [FIX] odoo_install: add missing Python libraries for enterprise install
    Odoo 13 introduced some new apps/dependencies that where not yet added. As the enterprise repository has no requirements.txt file I've added the extra dependencies under the pip3 command.
    These will only be installed if you've checked on the enterprise option in the script.

--- odoo_install.sh
+++ odoo_install.sh
@@ -126,7 +126,7 @@ if [ $IS_ENTERPRISE = "True" ]; then
 
     echo -e "\n---- Added Enterprise code under $OE_HOME/enterprise/addons ----"
     echo -e "\n---- Installing Enterprise specific libraries ----"
-    sudo pip3 install num2words ofxparse
+    sudo pip3 install num2words ofxparse dbfread ebaysdk firebase_admin pyOpenSSL
     sudo npm install -g less
     sudo npm install -g less-plugin-clean-css
 fi
