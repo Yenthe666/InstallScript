fe782ac4c9900690faf46e10edee53b0bad5510d
César Cordero Rodríguez <cesar.cordero.r@gmail.com>
2020-02-24 09:53:11 -0400

    Update odoo_install_debian.sh

--- odoo_install_debian.sh
+++ odoo_install_debian.sh
@@ -61,7 +61,7 @@ sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
 # Install Dependencies
 #--------------------------------------------------
 echo -e "\n--- Installing Python 3 + pip3 --"
-sudo apt-get install git python3 python3-pip build-essential wget python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less libpng16-16 gdebi -y
+sudo apt-get install git python3 python3-pip build-essential wget python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less libpng16-16 gdebi-core -y
 
 echo -e "\n---- Install python packages/requirements ----"
 sudo pip3 install -r https://github.com/odoo/odoo/raw/${OE_VERSION}/requirements.txt
