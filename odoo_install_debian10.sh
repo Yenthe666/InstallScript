c38e2bd1e0d9ff7d0860b738f9af3d0274c19b2d
César Cordero Rodríguez <cesar.cordero.r@gmail.com>
2020-02-24 10:00:47 -0400

    Update odoo_install_debian10.sh

--- odoo_install_debian10.sh
+++ odoo_install_debian10.sh
@@ -61,7 +61,7 @@ sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
 #--------------------------------------------------
 # Install Dependencies
 #--------------------------------------------------
-echo -e "\n--- Installing Python 3 + pip3 --"
+echo -e "\n--- Installing Python + pip --"
 sudo apt-get install wget git python python-pip build-essential wget python-dev python-wheel libxslt1-dev libzip-dev libldap2-dev libsasl2-dev python-setuptools node-less libpng16-16 gdebi-core -y
 
 echo -e "\n---- Install python packages/requirements ----"
