998e7c701c296eb02561d126f6aac8a67c741983
Carlitos Melgar <carlitos.melgar@izote.net>
2020-04-27 19:10:25 -0400

    Please read this to understand these changes. https://raymii.org/s/tutorials/Strong_SSL_Security_On_nginx.html

--- odoo_install.sh
+++ odoo_install.sh
@@ -402,13 +402,14 @@ if [ $PROXY_MODE = "PROXY_LETSENCRYPT" ] && [ $ADMIN_EMAIL != "odoo@example.com"
 
   cat <<EOF >/etc/nginx/snippets/ssl.conf
 ssl_dhparam                                    /etc/ssl/certs/dhparam.pem;
+ssl_ecdh_curve                                 secp384r1;
 
 ssl_session_timeout                            1d;
 ssl_session_cache                              shared:SSL:50m;
 ssl_session_tickets                            off;
 
-ssl_protocols                                  TLSv1 TLSv1.1 TLSv1.2;
-ssl_ciphers                                    'ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:ECDHE-ECDSA-DES-CBC3-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:DES-CBC3-SHA:!DSS';
+ssl_protocols                                  TLSv1.2 TLSv1.3;
+ssl_ciphers                                    ECDHE-RSA-AES256-GCM-SHA512:DHE-RSA-AES256-GCM-SHA512:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-SHA384;
 ssl_prefer_server_ciphers                      on;
 
 ssl_stapling                                   on;
